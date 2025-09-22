ab9e3a5c551ac53d9f73ef74f2f6febf6dc658b6
Sandra Garcia Martinez <sangarci@c4r6s2.42urduliz.com>
2025-09-22 15:37:58 +0200

    directorio libreria

---
+++ libft/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangarci <sangarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 11:44:00 by sangarci          #+#    #+#             */
+/*   Updated: 2025/09/22 11:53:18 by sangarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* #include <stdio.h> */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/* int	main(void)
+{
+	char str[] = "Sandra";
+	int	len = ft_strlen(str);
+	printf("%i", len);
+	return (0);
+} */
---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangarci <sangarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 12:28:07 by sangarci          #+#    #+#             */
+/*   Updated: 2025/09/22 13:02:29 by sangarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Guarda y directiva de la librería */
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/* Prototipos de las funciones de la libreria */
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *str);
+
+#endif
